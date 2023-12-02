b3bbe391dbf37da887ba319f67c412ee2d7afcab
s01k1m <sk618dev@gmail.com>
2023-12-03 01:58:46 +0900

    feat: next.js 13 /app에 맞게 업데이트

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,39 +1,39 @@
 // import Container from '../../../components/Container'
 import { allPosts } from '@/contentlayer/generated'
 // import { InferGetStaticPropsType } from 'next'
-import { useMDXComponent } from 'next-contentlayer/hooks'
+import { getMDXComponent } from 'next-contentlayer/hooks'
 
-const Post = ({ post }: any) => {
-  const MDXComponent = useMDXComponent(post.body.code)
+// const Post = ({ post }: any) => {
+//   const MDXComponent = useMDXComponent(post.body.code)
 
-  const customMeta = {
-    title: post.title,
-    description: post.description,
-    date: new Date(post.createdAt).toISOString(),
-  }
+//   const customMeta = {
+//     title: post.title,
+//     description: post.description,
+//     date: new Date(post.createdAt).toISOString(),
+//   }
 
-  return (
-    <>
-      <div className="prose mt-10">
-        <h1 className="text-center text-[32px] font-bold">{post.title}</h1>
-        <hr className="m-5 h-1" />
-        <MDXComponent />
-      </div>
-    </>
-  )
-}
+//   return (
+//     <>
+//       <div className="prose mt-10">
+//         <h1 className="text-center text-[32px] font-bold">{post.title}</h1>
+//         <hr className="m-5 h-1" />
+//         <MDXComponent />
+//       </div>
+//     </>
+//   )
+// }
 
-// Return a list of `params` to populate the [slug] dynamic segment
-export async function generateStaticParams() {
-  // const posts = await fetch('https://.../posts').then((res) => res.json())
+// // Return a list of `params` to populate the [slug] dynamic segment
+// export async function generateStaticParams() {
+//   // const posts = await fetch('https://.../posts').then((res) => res.json())
 
-  return allPosts.map((post) => ({
-    slug: post._raw.flattenedPath,
-  }))
-}
+//   return allPosts.map((post) => ({
+//     slug: post._raw.flattenedPath,
+//   }))
+// }
 
-// Multiple versions of this page will be statically generated
-// using the `params` returned by `generateStaticParams`
+// // Multiple versions of this page will be statically generated
+// // using the `params` returned by `generateStaticParams`
 
 // export const getStaticPaths = async () => {
 //   return {
@@ -50,5 +50,30 @@ export async function generateStaticParams() {
 //     },
 //   }
 // }
+
+// export default Post
+
+export const generateStaticParams = async () =>
+  allPosts.map((post) => ({ slug: post._raw.flattenedPath }))
+
+export const generateMetadata = ({ params }) => {
+  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug)
+  return { title: post.title }
+}
+
+const Post = ({ params }: { params: { slug: string } }) => {
+  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug)
+
+  const Content = getMDXComponent(post.body.code)
+
+  return (
+    <article className="mx-auto max-w-xl py-8">
+      <div className="mb-8 text-center">
+        <h1>{post?.title}</h1>
+      </div>
+      <Content />
+    </article>
+  )
+}
 
 export default Post
