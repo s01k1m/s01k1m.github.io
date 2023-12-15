5d9d92d21683994d221759ee2eaf65a1f2822837
s01k1m <sk618dev@gmail.com>
2023-12-16 02:56:15 +0900

    design: 클릭한 태그 디자인 추가

--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -1,18 +1,29 @@
+'use client'
+
 import { allPosts } from '@/contentlayer/generated'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 interface PropsType {
   tagData: object
 }
 
 const Tags = ({ tagData }: PropsType) => {
-  const styleTag: string =
-    'relative ml-2 rounded-full border-[2px] border-solid border-[#999] p-2 px-4 pr-5 hover:border-black hover:bg-black hover:text-white'
+  const pathname = usePathname()
+  console.log(pathname)
+  const isActiveTag = (tag: string) => {
+    if (tag === '/tags') return pathname === tag
+
+    return pathname.includes(tag)
+  }
 
   return (
     <div className="flex">
       <Link href="/tags/">
-        <div className={styleTag}>
+        <div
+          className={`relative ml-2 rounded-full border-[2px] border-solid border-[#999] p-2 px-4 pr-5 hover:border-black hover:bg-black hover:text-white
+          ${isActiveTag('/tags') ? 'border-black bg-black text-white' : ''} `}
+        >
           <span>All</span>
           <span className="absolute ml-1 text-[10px] text-[#999]">
             {allPosts.length}
@@ -22,7 +33,10 @@ const Tags = ({ tagData }: PropsType) => {
       {Object.keys(tagData).map((key, index) => {
         return (
           <Link key={index} href={`/tags/${key}`}>
-            <div className={styleTag}>
+            <div
+              className={`relative ml-2 rounded-full border-[2px] border-solid border-[#999] p-2 px-4 pr-5 hover:border-black hover:bg-black hover:text-white
+          ${isActiveTag(`/${key}`) ? 'border-black bg-black text-white' : ''} `}
+            >
               <span>{key}</span>
               <span className="absolute ml-1 text-[10px] text-[#999]">
                 {tagData[key]}
